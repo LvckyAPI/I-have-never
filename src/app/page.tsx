eefc50ba0f8f97a4228383caee2f72971e2f6e83
Iven Schlenther <iven.s@lvckyworld.net>
2023-12-08 01:45:13 +0100

    [TASK] center the box in the middle on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,10 @@ export default function Main() {
   return (
     <>
       <section className="w-full h-screen p-6 md:p-12 bg-gray-900 text-center text-white">
-          <h1 className="text-4xl md:text-6xl font-bold mb-6">I Have Never</h1>
-          <GameComponent />
+          <div className="h-full flex flex-col justify-center items-center sm:h-fit sm:block">
+              <h1 className="text-4xl md:text-6xl font-bold mb-6">I Have Never</h1>
+              <GameComponent />
+          </div>
       </section>
 
        <footer className="fixed bottom-1 w-full p-6 md:p-12 bg-black text-center text-white mt-12">
